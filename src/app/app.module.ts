330106ec2ef1ac9894c4c258e568dbd0f7738c94
Kevin <KevinSolimo@gmail.com>
2019-02-18 12:24:37 +0000

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; //Aggiungiamo qui 
+
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
